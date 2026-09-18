bd2731633de4abe41022ceb91f35818e3346f3e3
xyczhang <chloezha@andrew.cmu.edu>
2026-09-18 17:22:53 -0400

    Delete Vector Flight Tracker directory

--- Vector Flight Tracker/api/flights.js
+++
@@ -1,8 +0,0 @@
-import { handleFlights, json } from "./_shared.js";
-
-export default {
-  async fetch(request) {
-    if (request.method !== "GET") return json({ error: "Method not allowed" }, 405);
-    return handleFlights(request);
-  },
-};
